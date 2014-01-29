797d6e101cfb9000419ceb1410a588f5df652eb3
stevenmatthewt <sthomas85@gatech.edu>
2014-01-29 14:30:33 -0500

    Clicking "Login" button switches to blank login activity

--- MoneyManager/src/com/serialcoders/moneymanager/Startup.java
+++ MoneyManager/src/com/serialcoders/moneymanager/Startup.java
@@ -2,7 +2,9 @@ package com.serialcoders.moneymanager;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class Startup extends Activity {
 
@@ -17,6 +19,11 @@ public class Startup extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.startup, menu);
 		return true;
+	}
+	
+	public void switchToLogin(View view) {
+		Intent intent = new Intent(getApplicationContext(), Login.class);
+	    startActivity(intent);
 	}
 
 }
