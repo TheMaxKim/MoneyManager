76d7fca711b1ebeea3c74f635e81fe8561a91a52
tli61 <tli61@gatech.edu>
2014-01-30 15:04:25 -0500

    Added a splash screen
    Splash screen logo can be changed, I just made a crappy one for now.

---
+++ MoneyManager/src/com/serialcoders/moneymanager/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.serialcoders.moneymanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+ 
+public class SplashScreen extends Activity {
+ 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+
+ 
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, Startup.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
+}
