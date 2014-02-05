a169a5fc26ffc51dd6bac90b1a70cae37001f60f
TheMaxKim <maxjukim@gmail.com>
2014-02-05 14:11:34 -0500

    Added basic login check

--- MoneyManager/src/com/serialcoders/moneymanager/Login.java
+++ MoneyManager/src/com/serialcoders/moneymanager/Login.java
@@ -3,9 +3,14 @@ package com.serialcoders.moneymanager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends Activity {
-
+	EditText username;
+	EditText password;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,19 @@ public class Login extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.login, menu);
         return true;
+    }
+    
+    public void login(View view) {
+    	EditText username = (EditText)findViewById(R.id.username);
+    	EditText password = (EditText)findViewById(R.id.password);
+    	if (username.getText().toString().equals("admin") &&
+    		password.getText().toString().equals("pass123")) {
+    	    	Toast.makeText(getApplicationContext(), "Success!", 
+    	    	      Toast.LENGTH_SHORT).show();
+    	} else {
+    	      Toast.makeText(getApplicationContext(), "Incorrect Login Credentials", 
+    	    	      Toast.LENGTH_SHORT).show();
+    	}
     }
     
 }
