4a4195d23b99e104825911246ac82f75b759c369
stevenmatthewt <sthomas85@gatech.edu>
2014-02-17 16:03:28 -0500

    Admin account now has a log out button

--- MoneyManager/src/com/serialcoders/moneymanager/AdminAccountActivity.java
+++ MoneyManager/src/com/serialcoders/moneymanager/AdminAccountActivity.java
@@ -1,15 +1,24 @@
 package com.serialcoders.moneymanager;
 
+import com.parse.ParseUser;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class AdminAccountActivity extends Activity {
+	
+	private ParseUser user;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_admin_account);
+		
+		user = ParseUser.getCurrentUser();
 	}
 
 	@Override
@@ -17,6 +26,14 @@ public class AdminAccountActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.admin_account, menu);
 		return true;
+	}
+	
+	public void logOut(View v) {
+		user.logOut();
+		
+		Toast.makeText(AdminAccountActivity.this, "Successfully logged out!", Toast.LENGTH_LONG).show();
+		Intent in = new Intent(AdminAccountActivity.this, Login.class);
+		startActivity(in);
 	}
 
 }
