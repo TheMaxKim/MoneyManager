35cacf5557e82db3a7cb47d04992d6bd6134ceac
TheMaxKim <maxjukim@gmail.com>
2014-02-27 00:40:48 -0500

    Created a check to prevent financial account creation when another account with the same full name of display name already exists. Added a placeholder remove account button. Changed list of accounts from static text to a list of buttons so that in the future each account can be accessed from the user account activity. Changed some the look of some buttons to try out.

--- MoneyManager/src/com/serialcoders/moneymanager/CreateAccount.java
+++ MoneyManager/src/com/serialcoders/moneymanager/CreateAccount.java
@@ -1,9 +1,12 @@
 package com.serialcoders.moneymanager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import android.os.Bundle;
@@ -20,6 +23,8 @@ public class CreateAccount extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_account);
+		
+		Parse.initialize(this, "f0ZnpLcS3ysYplTiCoBOGKz3jFsdcGX9y5n3GLIT", "dZ5kg5BmoWFf5YdCBrDrcjZ7QA4SU5qSg8C151f3");
 	}
 
 	@Override
@@ -36,18 +41,48 @@ public class CreateAccount extends Activity {
         String displayName = displayNameView.getText().toString();
         
         ParseUser user = ParseUser.getCurrentUser();
-		ParseObject account = new ParseObject("Account");
-		account.put("fullName", fullName);
-		account.put("displayName", displayName);
-		account.put("username", user.get("username"));
-		account.saveInBackground();
-		
-		
-		user.add("Accounts", account);
-		Toast.makeText(CreateAccount.this, "Account Created", Toast.LENGTH_LONG).show();
-		
-		Intent i = new Intent(CreateAccount.this, UserAccountActivity.class);
-		startActivity(i);
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
+        List<ParseObject> accountList;
+        
+        
+        try {
+	    	accountList = query.find();
+	    } catch (ParseException e) {
+	    	accountList = new ArrayList<ParseObject>();
+	    }
+        
+    	boolean accountExists = false;
+        
+        if ((fullName.equals("")) || (displayName.equals(""))) {
+        	Toast.makeText(CreateAccount.this, "Your new account must have both a name and display name!", Toast.LENGTH_SHORT).show();
+        } else {
+	        for (ParseObject a : accountList) {
+	            	String accountFullName = a.getString("fullName");
+	            	String accountDisplayName = a.getString("displayName");
+	            	if ((fullName.equals(accountFullName))) {
+	            		Toast.makeText(CreateAccount.this, "An account with this name already exists!", Toast.LENGTH_LONG).show();
+	            		accountExists = true;
+	            	} else if ((displayName.equals(accountDisplayName))) {
+	            		Toast.makeText(CreateAccount.this, "An account with this display name already exists!", Toast.LENGTH_LONG).show();
+	            		accountExists = true;
+	            	}
+	            }
+	        
+	        if (!accountExists) {
+				ParseObject account = new ParseObject("Account");
+				account.put("fullName", fullName);
+				account.put("displayName", displayName);
+				account.put("username", user.get("username"));
+				account.saveInBackground();
+			
+			
+				user.add("Accounts", account);
+				Toast.makeText(CreateAccount.this, "Account Created", Toast.LENGTH_LONG).show();
+				
+				Intent i = new Intent(CreateAccount.this, UserAccountActivity.class);
+				startActivity(i);
+	    	}
+        }
 	}
 
 }
--- MoneyManager/src/com/serialcoders/moneymanager/UserAccountActivity.java
+++ MoneyManager/src/com/serialcoders/moneymanager/UserAccountActivity.java
@@ -3,6 +3,7 @@ package com.serialcoders.moneymanager;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -12,8 +13,10 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,6 +39,7 @@ public class UserAccountActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
+		ScrollView sv = new ScrollView(this);
 	    setContentView(R.layout.activity_useraccount);
 	    
 	    Parse.initialize(this, "f0ZnpLcS3ysYplTiCoBOGKz3jFsdcGX9y5n3GLIT", "dZ5kg5BmoWFf5YdCBrDrcjZ7QA4SU5qSg8C151f3");
@@ -44,7 +48,7 @@ public class UserAccountActivity extends Activity {
 	    
 	    TextView textView = (TextView) findViewById(R.id.welcomemessage);
 	    String username = user.getUsername();
-	    textView.setText("Welcome to MoneyManager " + username + "!");
+	    textView.setText("Welcome to MoneyManager, " + username + "!");
 	    
 	    List<ParseObject> accountList;
 	    ParseQuery<ParseObject> query = ParseQuery.getQuery("Account");
@@ -57,11 +61,23 @@ public class UserAccountActivity extends Activity {
 	    }
 	    
 	    for (ParseObject a : accountList) {
-	    	textView = new TextView(UserAccountActivity.this);
+	    	/*textView = new TextView(UserAccountActivity.this);			//Old code for just listing the names of the accounts.
 	    	textView.setText(a.getString("displayName"));
 	    	LinearLayout ll = (LinearLayout) findViewById(R.id.account_list);
 	    	
-	    	ll.addView(textView);
+	    	ll.addView(textView);*/
+	    	Button accountButton = new Button(UserAccountActivity.this);
+	    	accountButton.setText(a.getString("displayName"));
+	    	LinearLayout ll = (LinearLayout)findViewById(R.id.account_list);
+            LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+            ll.addView(accountButton, lp);
+            
+            accountButton.setOnClickListener(new View.OnClickListener() {
+            	public void onClick(View view) {
+            		Intent in = new Intent(UserAccountActivity.this, FinancialAccount.class);
+            		startActivity(in);
+            	}
+            });
 	    }
 	    
 	    findViewById(R.id.button_logout).setOnClickListener(new View.OnClickListener() {
@@ -72,14 +88,21 @@ public class UserAccountActivity extends Activity {
         		Toast.makeText(UserAccountActivity.this, "Successfully logged out!", Toast.LENGTH_LONG).show();
         		Intent in = new Intent(UserAccountActivity.this, Login.class);
 				startActivity(in);
-				
-				
+
         	}
         });
+		
+		
 	}
 	
 	public void addAccount(View v) {
 		Intent i = new Intent(UserAccountActivity.this, CreateAccount.class);
 		startActivity(i);
 	}
+
+	
+	public void removeAccount(View v) {
+		//TODO Add remove account functionality for button
+	}
+	
 }
