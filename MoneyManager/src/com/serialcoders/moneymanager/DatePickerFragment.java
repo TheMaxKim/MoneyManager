ddc761b3c684fdff65e52b8cda70b43d6df5cbcb
stevenmatthewt <sthomas85@gatech.edu>
2014-03-07 21:28:26 -0500

    added a Spending Report. (Still WIP)

---
+++ MoneyManager/src/com/serialcoders/moneymanager/DatePickerFragment.java
@@ -0,0 +1,41 @@
+package com.serialcoders.moneymanager;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class DatePickerFragment extends DialogFragment
+								implements DatePickerDialog.OnDateSetListener {
+	
+		final Calendar c = Calendar.getInstance();
+		Date date;
+		EditText editText;
+		
+		public Dialog onCreateDialog(Bundle SavedInstanceState) {
+			Log.d("tag", getArguments().toString());
+			editText = (EditText) getActivity().findViewById(getArguments().getInt("EditText"));
+			
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+			
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+		
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			c.set(year, month, day);
+			date = c.getTime();
+			DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+			editText.setText(df.format(date));
+		}
+}
--- MoneyManager/src/com/serialcoders/moneymanager/SpendingReportActivity.java
+++ MoneyManager/src/com/serialcoders/moneymanager/SpendingReportActivity.java
@@ -3,6 +3,7 @@ package com.serialcoders.moneymanager;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,15 +15,28 @@ import com.parse.ParseUser;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ActionBar.LayoutParams;
+import android.app.DialogFragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class SpendingReportActivity extends Activity {
+	
+	Date dateFrom, dateTo;
+	Calendar c;
+	ParseUser user;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +45,121 @@ public class SpendingReportActivity extends Activity {
 		// Show the Up button in the action bar.
 		setupActionBar();
 		
-		ParseUser user = ParseUser.getCurrentUser();
+		user = ParseUser.getCurrentUser();
+		dateTo = new Date();
+		dateFrom = new Date(0);
 		
+		EditText dateFromText = (EditText) findViewById(R.id.date_from);
+		dateFromText.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				DialogFragment dateFragment = new DatePickerFragment();
+				Bundle bundle = new Bundle(1);
+				bundle.putInt("EditText", R.id.date_from);
+				dateFragment.setArguments(bundle);
+			    dateFragment.show(getFragmentManager(), "datePicker");
+			}
+		});
+		dateFromText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable v) {
+				DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+				try {
+					dateFrom = dateFormat.parse(v.toString());
+				} catch (java.text.ParseException e) {
+					dateFrom = new Date();
+					Log.d("error1", e.getMessage());
+//					Toast.makeText(getContext(), "Please try entering a new date" + e, Toast.LENGTH_LONG).show();
+				}
+				findWithdrawals();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		EditText dateToText = (EditText) findViewById(R.id.date_to);
+		dateToText.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				DialogFragment dateFragment = new DatePickerFragment();
+				Bundle bundle = new Bundle(1);
+				bundle.putInt("EditText", R.id.date_to);
+				dateFragment.setArguments(bundle);
+			    dateFragment.show(getFragmentManager(), "datePicker");
+			}
+		});
+		dateToText.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void afterTextChanged(Editable v) {
+				DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT);
+				try {
+					dateTo = dateFormat.parse(v.toString());
+				} catch (java.text.ParseException e) {
+					dateTo = new Date();
+					Log.d("error1", e.getMessage());
+//					Toast.makeText(getContext(), "Please try entering a new date" + e, Toast.LENGTH_LONG).show();
+				}
+				findWithdrawals();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				
+				
+			}
+			
+		});
+	}
+	
+	private void findWithdrawals() {
 		//Find all Transactions which are withdrawals
+		Log.d("tag2", "hit this");
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Transaction");
 	    List<ParseObject> transactionList;
-        //query.whereEqualTo("accountFullName", passedName);
         query.whereEqualTo("userName", user);
         query.whereLessThan("amount", 0); //only get negative amounts
+        Log.d("tag3", dateFrom.toString());
+        Log.d("tag3", dateTo.toString());
+        query.whereGreaterThan("createdAt", dateFrom);
+        query.whereLessThan("createdAt", dateTo);
 	    try {
 	    	transactionList = query.find();
 	    } catch (ParseException e) {
 	    	transactionList = new ArrayList<ParseObject>();
 	    	Toast.makeText(this, "Cannot load transactions: " + e, Toast.LENGTH_LONG).show();
 	    }
-	    /*Double totalBalance = 0.0;
-	    if(accountInitialBalance != null){
-	    	totalBalance = Double.parseDouble(accountInitialBalance);
-	    }*/
+	    
 	    Double totalWithdrawals = 0.0;
 	    DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+	    LinearLayout ll = (LinearLayout)findViewById(R.id.withdrawal_list);
+    	ll.removeAllViews();
 	    for (ParseObject o : transactionList) {
 	    	Button accountButton = new Button(this);
 	    	Date transactionDate = o.getCreatedAt();    	
 	    	accountButton.setText(o.get("transactionType") + "   " + Double.toString(o.getDouble("amount")) + " on " + df.format(transactionDate));
 	    	accountButton.setBackgroundResource(R.drawable.green_button);
-	    	LinearLayout ll = (LinearLayout)findViewById(R.id.withdrawal_list);
+	    	
             LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
             ll.addView(accountButton, lp);
             
