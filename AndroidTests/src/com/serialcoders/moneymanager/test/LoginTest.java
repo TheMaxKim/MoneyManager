3d21d57d911c61ece12b8b5ac5f0618cdcf80b74
TheMaxKim <maxjukim@gmail.com>
2014-04-02 18:04:31 -0400

    Added Login JUnit Test

---
+++ AndroidTests/src/com/serialcoders/moneymanager/test/LoginTest.java
@@ -0,0 +1,48 @@
+package com.serialcoders.moneymanager.test;
+
+import com.parse.ParseUser;
+import com.serialcoders.moneymanager.Login;
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginTest extends ActivityInstrumentationTestCase2<Login>{
+
+	Activity activity;
+	EditText usernameView, passwordView;
+	Button loginButton;
+	ParseUser user;
+	
+	public LoginTest() {
+		super(Login.class);
+
+	}
+	@UiThreadTest
+	protected void setUp() throws Exception {
+		
+		
+		super.setUp();
+		
+		activity = getActivity();
+		
+		
+		
+		usernameView = (EditText) activity.findViewById(com.serialcoders.moneymanager.R.id.username);
+		passwordView = (EditText) activity.findViewById(com.serialcoders.moneymanager.R.id.password);
+		
+		loginButton = (Button) activity.findViewById(com.serialcoders.moneymanager.R.id.button_logIn);
+	}
+	@UiThreadTest
+	public void testLogin() {
+		
+		usernameView.setText("max");
+		passwordView.setText("hello");
+		
+		loginButton.performClick();
+		assertEquals(ParseUser.getCurrentUser().getUsername(), "max");
+	}
+	
+	
+}
