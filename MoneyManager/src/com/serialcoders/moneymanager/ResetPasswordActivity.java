2a07219b631d26f3c2112c5c6b724da21edf08eb
tli61 <tli61@gatech.edu>
2014-04-24 04:05:49 -0400

    more pop up window

--- MoneyManager/src/com/serialcoders/moneymanager/ResetPasswordActivity.java
+++ MoneyManager/src/com/serialcoders/moneymanager/ResetPasswordActivity.java
@@ -4,12 +4,17 @@ import java.util.logging.Logger;
 
 import org.apache.commons.logging.Log;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -33,10 +38,7 @@ public class ResetPasswordActivity extends Activity {
         //getActionBar().setDisplayShowTitleEnabled(false);
         
         button_forgot_password = (Button) findViewById(R.id.resetPassword);
-       
-        
-        
-      
+                      
         button_forgot_password.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -44,7 +46,8 @@ public class ResetPasswordActivity extends Activity {
 				forgotpw_email = (EditText) findViewById(R.id.forgot_password_userEmail);
 				accountEmail = forgotpw_email.getText().toString();
 				checkEmailID();
-				 
+				
+				
 				
 			}
 		});
@@ -74,7 +77,24 @@ public class ResetPasswordActivity extends Activity {
 		@Override
 		public void done(ParseException e) {
 			if (e == null) {
-	            Toast.makeText(getApplicationContext(), "Successfully sent link to your email for reset Password", Toast.LENGTH_LONG).show();
+				LayoutInflater layoutInflater 
+		        = (LayoutInflater)getBaseContext()
+		         .getSystemService(LAYOUT_INFLATER_SERVICE);  
+		       View popupView = layoutInflater.inflate(R.layout.pop_up_window, null);  
+		                final PopupWindow popupWindow = new PopupWindow(
+		                  popupView, 
+		                  LayoutParams.WRAP_CONTENT,  
+		                        LayoutParams.WRAP_CONTENT);  
+		                
+		                Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+		                btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+		        @Override
+		        public void onClick(View v) {
+		         // TODO Auto-generated method stub
+		         popupWindow.dismiss();
+		        }});
+		                popupWindow.showAsDropDown(button_forgot_password, 50, -30);
 	        } else {
 	            Toast.makeText(getApplicationContext(), "Failed to sent link to your email for reset Password", Toast.LENGTH_LONG).show();
 	            System.out.println(e);
